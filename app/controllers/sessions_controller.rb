db4fa76e021b14d8b7b8f93b0537aa2fcf435c48
ditsky <samruditsky@yahoo.com>
2020-05-20 15:33:40 -0400

    remember me function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       log_in user
+      params[:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def logout
-    log_out
+    log_out if logged_in?
     flash[:success] = "Logged out"
     redirect_to root_url
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_164905) do
+ActiveRecord::Schema.define(version: 2020_05_20_190057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_164905) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
