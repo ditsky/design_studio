a085b54f3e3cc9aa3ad282094d2c2a9a1e22ef9c
ditsky <samruditsky@yahoo.com>
2020-05-29 16:20:24 -0400

    image fix for heroku2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #Load images on heroku
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
 
   # Code is not reloaded between requests.
