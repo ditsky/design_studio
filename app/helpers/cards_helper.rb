cd0b455fae69bdc16c8eec301bc24aaa41f91bd3
ditsky <samruditsky@yahoo.com>
2020-10-30 18:05:22 -0400

    price fix

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -11,6 +11,25 @@ module CardsHelper
         end
     end
 
+    #Converts int prices into proper dollar string amount
+    def price_to_string(price)
+        if (!price)
+            return "0"
+        end
+        price = price.round(2)
+        price_string = price.to_s
+        first_cent_index = price_string.index(".") + 1
+        cent_len = price_string.length - first_cent_index
+        if cent_len < 2
+            price_string += "0"
+        elsif cent_len > 2
+            price_string = price_string[0,first_cent_index + 2]
+
+        end
+
+        return price_string
+    end
+
 
 
 end
