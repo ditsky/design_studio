7714097e6f8a84f4addffa8f92bbf9f2f2235fe6
ditsky <samruditsky@yahoo.com>
2020-11-06 15:46:13 -0500

    trying ui changes on home2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,11 +8,11 @@ class PagesController < ApplicationController
   end
 
   def home
-    @birthday_cards = Card.where(content: "birthday").first(2)
+    @birthday_cards = Card.where(content: "birthday", card_type: "post card").first(2)
     # HACKY hack hack, fix this later
-    @sympathy_cards = Card.where(content: "love", card_type: "fold over").first(2)
-    @thank_you_cards = Card.where(content: "thank you").first(2)
-    @any_occasion_cards = Card.where(content: "any occasion").first(2)
+    @sympathy_cards = Card.where(content: "love", card_type: "post card").first(2)
+    @thank_you_cards = Card.where(content: "thank you", card_type: "post card").first(2)
+    @any_occasion_cards = Card.where(content: "any occasion", card_type: "post card").first(2)
     @cards = [@birthday_cards, @thank_you_cards, @any_occasion_cards, @sympathy_cards]
   end
 
