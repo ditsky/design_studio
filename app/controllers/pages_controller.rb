594b8401cadac8ac69fee30c3781f529bd771e5a
ditsky <samruditsky@yahoo.com>
2020-11-06 16:14:26 -0500

    more breakpoints and db hacks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,14 @@ class PagesController < ApplicationController
   def home
     @birthday_cards = Card.where(content: "birthday", card_type: "post card").first(2)
     # HACKY hack hack, fix this later
-    @sympathy_cards = Card.where(content: "love", card_type: "post card").first(2)
+    if Card.where(content: "love", card_type: "post card").first(2).size == 2
+      @sympathy_cards = Card.where(content: "love", card_type: "post card").first(2)
+    elsif Card.where(content: "love", card_type: "fold over").first(2) == 2
+      @sympathy_cards = Card.where(content: "love", card_type: "fold over").first(2)
+    else
+      @sympathy_cards = Card.where(content: "love").first(2)
+    end
+    
     @thank_you_cards = Card.where(content: "thank you", card_type: "post card").first(2)
     @any_occasion_cards = Card.where(content: "any occasion", card_type: "post card").first(2)
     @cards = [@birthday_cards, @thank_you_cards, @any_occasion_cards, @sympathy_cards]
