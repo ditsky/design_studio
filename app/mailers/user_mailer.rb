fe56c5cc3fe51759c819fe4994ae2279692a9792
ditsky <samruditsky@yahoo.com>
2021-02-01 15:43:05 -0500

    done for now

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -20,19 +20,6 @@ class UserMailer < ApplicationMailer
   def password_reset(user)
     @user = user
     headers['X-Entity-Ref-ID'] = "2"
-    headers["X-SMTPAPI"] = JSON.generate({
-      :sub => {
-        '%name' => [user.email]
-      },
-      :filters => {
-        :templates => {
-          :settings => {
-            :enable => 1,
-            :template_id => 'd-2eeb9c9ab0234695908aacdee26708cf'
-          }
-        }
-      }
-    })
     mail to: user.email, subject: "Password reset"
   end
 
