630d39c39156bd06256a025acef2df00dcadf7fa
ditsky <samruditsky@yahoo.com>
2021-02-01 13:43:58 -0500

    newsletter spacing

--- app/javascript/packs/newsletter.js
+++ app/javascript/packs/newsletter.js
@@ -2,6 +2,6 @@ var preview = document.getElementById("preview")
 
 preview.onclick = (event) => {
     event.preventDefault()
-    var body = document.getElementById("email_body")
-    window.open("/rails/mailers/user_mailer/newsletter?email_body="+ body.value, 'Preview', "_parent");
+    var body = document.getElementById("email_body").value.replace(/(?:\r\n|\r|\n)/g, '<br>').replace(/ /g, '\u00a0');
+    window.open("/rails/mailers/user_mailer/newsletter?email_body="+ body, 'Preview', "_parent");
 }
